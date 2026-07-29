80a7c173c351d715a82d8f30b46d318139b6262a
chaudharikirtesh90 <chaudharikirtesh90@gmail.com>
2026-07-29 17:21:48 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cd /home/ec2-user/app || mkdir -p /home/ec2-user/app && cd /home/ec2-user/app
+cd /home/ubuntu/frontend_app
 
 if [ ! -d .git ]; then
   echo "Repository not found. Clone or upload the app first."
